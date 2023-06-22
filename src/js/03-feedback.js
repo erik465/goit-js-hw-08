18c3e3c6d3caf70cdc78ff7db693d6dc8b3c196a
erik465 <ehalasz06@gmail.com>
2023-06-22 13:23:54 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -15,10 +15,16 @@ formElement.addEventListener('input', throttle(() => {
         email : formElement.firstElementChild.firstElementChild.value,
         message : formElement.children[1].lastElementChild.value
     }))
+    console.log(localStorage.getItem('feedback-form-state'))
 }, 500))
 
 formElement.addEventListener('submit', (e) =>{
     e.preventDefault()
+    localStorage.setItem('feedback-form-state', JSON.stringify({
+        email : formElement.firstElementChild.firstElementChild.value,
+        message : formElement.children[1].lastElementChild.value
+    }))
     console.log(JSON.parse(localStorage.getItem('feedback-form-state')))
+    formElement.reset()
     localStorage.clear()
 })
